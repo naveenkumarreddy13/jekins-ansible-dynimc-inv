af50e9234a650c7fb88f66957b196e686fbd75a4
naveenkumarreddy13 <139735589+naveenkumarreddy13@users.noreply.github.com>
2023-09-09 14:36:17 +0530

    Update aws_ec2.tf

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -1,14 +1,13 @@
-provider "aws"{
-region = "ap-south-1"
+provide "aws"{
+region="ap-south-1"
 }
-
 
 resource "aws_instance" "AWSServer"{
 ami = "ami-02bb7d8191b50f4bb"
 instance_type = "t2.micro"
 security_groups = ["launch-wizard-2"]
-key_name = "devopsmithuntechnologies"
-tags = {
-Name =  "tomcatservers"
+key_name = "mithuntechnologiesdevopsnaveen"
+tags={
+Name = "tomcatservers"
 }
 }
